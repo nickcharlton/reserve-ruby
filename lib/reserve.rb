d465b2f6caafbb8a17f6fc09127ebc90099d4313
Nick Charlton <hello@nickcharlton.net>
2014-09-24 23:02:33 +0100

    Implements a module-level initialisation helper.

--- lib/reserve.rb
+++ lib/reserve.rb
@@ -2,5 +2,8 @@ require "reserve/version"
 require "reserve/store"
 
 module Reserve
-
+  # Create a new Reserve instance.
+  def self.new redis, opts={}
+    Reserve::Store.new redis, opts
+  end
 end
